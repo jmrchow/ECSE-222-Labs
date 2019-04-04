5a6b549a80ade6b2c723be634bb5f93777eb0b2c
cboustros <46546409+cboustros@users.noreply.github.com>
2019-04-04 10:53:48 -0400

    Update g61_clock_divider.vhd

--- g61_clock_divider.vhd
+++ g61_clock_divider.vhd
@@ -15,10 +15,10 @@ architecture a0 of g61_clock_divider is
 		Port (enable : in std_logic;
 			clk : in std_logic;
 			reset : in std_logic;
-			count : out natural range 0 to 499999);
+			count : out natural range 0 to 499998);
 	end component g61_downcounter;
 	
-	signal count : natural range 0 to 499999;
+	signal count : natural range 0 to 499998;
 	
 begin 
 	downcounter0 : g61_downcounter port map (enable, clk, reset, count);
