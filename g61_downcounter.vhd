4945f34586b0189b4eb5bea89f5d2626d7c1ace3
cboustros <46546409+cboustros@users.noreply.github.com>
2019-04-04 11:43:28 -0400

    Update g61_downcounter.vhd

--- g61_downcounter.vhd
+++ g61_downcounter.vhd
@@ -5,27 +5,27 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity g61_downcounter is
-	Port (enable : in std_logic;
-			clk : in std_logic;
-			reset : in std_logic;
-			count : out natural range 0 to 499998);
+	Port (	enable : in std_logic; -- this one-bit input determines whether the downcounter is paused or unpaused
+		clk : in std_logic; -- this one-bit input indicates when a single clock cycle has occured
+		reset : in std_logic; -- this one-bit input determines whether the count will be reset or not
+		count : out natural range 0 to 499998); -- a natural number equal to the current count is returned as output
 end g61_downcounter;
 
 architecture a0 of g61_downcounter is
-	signal temp: natural range 0 to 499998;
+	signal temp: natural range 0 to 499998; -- a temporary variable is created because the count variable cannot be modified inside inside an if-statement
 begin
 	Process(clk, reset) begin
-		if (reset = '0') then
-			temp <= 499998;
-		elsif(rising_edge(clk)) then
-			if (enable = '1') then
-				if (temp = 0) then
-					temp <= 499998;
+		if (reset = '0') then -- if reset is 0
+			temp <= 499998; -- reset the count
+		elsif(rising_edge(clk)) then -- if clk has changed from 1 to 0 or from 0 to 1
+			if (enable = '1') then -- if enable is 1 (downcounter is unpaused)
+				if (temp = 0) then -- if temp is 0
+					temp <= 499998; -- reset the count
 				else
-					temp <= temp - 1;
+					temp <= temp - 1; -- decrement temp
 				end if;
 			end if;
 		end if;
-		count <= temp;
+		count <= temp; -- store the value of temp as the output
 	end Process;
 end;
